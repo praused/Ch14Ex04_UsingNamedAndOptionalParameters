3610b895376aa5d89228762d3ff16ec971bec920
David Prause <praused@hotmail.com>
2017-09-20 09:37:36 -0400

    Example complete

--- Ch14Ex04_UsingNamedAndOptionalParameters/Program.cs
+++ Ch14Ex04_UsingNamedAndOptionalParameters/Program.cs
@@ -10,6 +10,28 @@ namespace Ch14Ex04_UsingNamedAndOptionalParameters
     {
         static void Main(string[] args)
         {
+            string sentence = "'twas brillig, and the slithy toves did gyre and gimble in the wabe:";
+            List<string> words;
+
+            words = WordProcessor.GetWords(sentence);
+            Console.WriteLine("Original sentence:");
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.Write(' ');
+
+            }
+            Console.WriteLine('\n');
+
+            words = WordProcessor.GetWords(sentence, reverseWords: true, capitalizeWords: true);
+            Console.WriteLine("Sentence with capitalized and reversed words:");
+            foreach (string word in words)
+            {
+                Console.Write(word);
+                Console.Write(' ');
+
+            }
+            Console.ReadKey();
         }
     }
 }
